24771627c22a04b45d3a6bdad01852963ba656e6
Your Name <you@example.com>
2020-05-03 23:59:06 -0300

    Fix

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,15 +23,26 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    async function processArray(): Promise<void> {
+      uploadedFiles.map(async (uploadedFile) => {
+        data.append('file', uploadedFile.file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+        try {
+          await api.post('/transactions/import', data);
+        } catch (err) {
+          console.log(err.response.error);
+        }
+
+        data.delete('file');
+
+      });
     }
+
+    await processArray();
+
+    history.push('/');
   }
 
   function submitFile(files: File[]): void {
